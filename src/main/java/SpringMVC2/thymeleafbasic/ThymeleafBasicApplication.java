4b6013ae1d48c4d653fc24c0bc9813c3499e9b61
JJH <jakebbak@gmail.com>
2023-05-09 12:40:40 +0900

    Initial commit

---
+++ src/main/java/SpringMVC2/thymeleafbasic/ThymeleafBasicApplication.java
@@ -0,0 +1,13 @@
+package SpringMVC2.thymeleafbasic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThymeleafBasicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThymeleafBasicApplication.class, args);
+	}
+
+}
